03d6e22ad0914018cac0bb6a89d6b6ed97b18b54
Chris Bentzel <cbentzel@chromium.org>
2017-09-04 21:24:31 -0400

    Change article template args

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/article/*', function(req, res, next) {
-  res.render('article', { title: 'Express' });
+  res.render('article', { title: 'Article' });
 });
 
 module.exports = router;
