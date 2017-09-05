1b26cef9eb0260f582d23a0ebc204d442b16426d
Chris Bentzel <cbentzel@chromium.org>
2017-09-04 21:22:02 -0400

    Router works on article subpath

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/article/', function(req, res, next) {
+router.get('/article/*', function(req, res, next) {
   res.render('article', { title: 'Express' });
 });
 
